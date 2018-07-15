c9382cd89ca256b3a19a299d580d913a6fb54ca3
Vouts <thevouts@gmail.com>
2018-07-15 11:51:50 +0300

    v1

--- app/src/main/java/com/example/android/inventoryapp/data/InventoryContract.java
+++ app/src/main/java/com/example/android/inventoryapp/data/InventoryContract.java
@@ -1,5 +1,6 @@
 package com.example.android.inventoryapp.data;
 
+import android.net.Uri;
 import android.provider.BaseColumns;
 
 public final class InventoryContract {
@@ -10,10 +11,39 @@ public final class InventoryContract {
     }
 
     /**
+     * The "Content authority" is a name for the entire content provider, similar to the
+     * relationship between a domain name and its website.  A convenient string to use for the
+     * content authority is the package name for the app, which is guaranteed to be unique on the
+     * device.
+     */
+    public static final String CONTENT_AUTHORITY = "com.example.android.inventoryapp";
+
+    /**
+     * Use CONTENT_AUTHORITY to create the base of all URI's which apps will use to contact
+     * the content provider.
+     */
+    public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
+
+    /**
+     * Possible path (appended to base content URI for possible URI's)
+     * For instance, content://com.example.android.pets/pets/ is a valid path for
+     * looking at pet data. content://com.example.android.pets/staff/ will fail,
+     * as the ContentProvider hasn't been given any information on what to do with "staff".
+     */
+
+    public static final String PATH_INVENTORY = "inventory";
+
+    /**
      * Inner class that defines constant values for the inventory database table.
      * Each entry in the table represents a single product.
      */
     public static final class InventoryEntry implements BaseColumns {
+
+        /**
+         * The content URI to access the pet data in the provider
+         */
+        public static final Uri CONTENT_URI = Uri.withAppendedPath(BASE_CONTENT_URI, PATH_INVENTORY);
+
 
         /**
          * Name of database table for products
